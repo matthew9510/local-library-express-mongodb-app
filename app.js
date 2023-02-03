79d27d88002183a5c9a6d4cfa6d19ffd92b4d92a
Matthew Hess <matthew9510@gmail.com>
2023-02-03 13:21:14 -0800

    init connect to mongodb using mongoose

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set('strictQuery', false);
+
+// Define the database URL to connect to.
+const dev_db_url = 'mongodb+srv://<username>:<password>@<clusterInfo>/<databaseName>?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// Wait for database to connect, logging an error if there is a problem 
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
